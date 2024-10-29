940c0eb30656151c83817595c0f5f7614cf33605
Milvookie <58269358+Milvookie@users.noreply.github.com>
2024-10-29 18:14:51 +0100

    create sketching area

--- main.js
+++ main.js
@@ -1,5 +1,34 @@
 const drawingArea = document.querySelector('#drawingArea')
 
 function createGrid() {
-    
+    for (let index = 0; index < 16; index++) {
+        let div = document.createElement('div')
+        div.className = 'grid-unit'
+        drawingArea.append(div)
+    }
 }
+
+createGrid()
+
+let gridUnits = document.querySelectorAll('.grid-unit')
+console.log(gridUnits);
+
+drawingArea.addEventListener('click', (e) => {
+    let posX = e.offsetX
+    let posY = e.offsetY
+    let target = e.target
+    console.log(target);
+    console.log(e);
+    
+})
+
+function draw() {
+
+}
+
+
+document.addEventListener('load', () => {
+
+    console.log('loaded');
+    
+})
