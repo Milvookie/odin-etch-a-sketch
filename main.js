e79179d9e544dd7d4a767a05a900ae0f0162bd18
Milvookie <58269358+Milvookie@users.noreply.github.com>
2024-10-30 14:20:28 +0100

    add hover function and input to change grid size

--- main.js
+++ main.js
@@ -1,34 +1,43 @@
 const drawingArea = document.querySelector('#drawingArea')
+const inputOfGridSize = document.getElementById('grid-size-input')
 
-function createGrid() {
-    for (let index = 0; index < 16; index++) {
-        let div = document.createElement('div')
-        div.className = 'grid-unit'
-        drawingArea.append(div)
-    }
-}
-
-createGrid()
-
-let gridUnits = document.querySelectorAll('.grid-unit')
-console.log(gridUnits);
-
-drawingArea.addEventListener('click', (e) => {
-    let posX = e.offsetX
-    let posY = e.offsetY
-    let target = e.target
-    console.log(target);
-    console.log(e);
+inputOfGridSize.addEventListener('change', (e) => {
+    console.log(e.target.value);
+    createGrid(e.target.value)
     
 })
 
-function draw() {
+function createGrid(num) {
+    const totalSquares = num * num
+    const areaHeight = drawingArea.clientHeight
+    const areaWidth = drawingArea.clientWidth
+    let gridElement = []
+    for (let index = 0; index < totalSquares; index++) {
+        let unit = document.createElement('div')
+        unit.className = 'grid-unit'
+        unit.style.height = areaHeight / num + 'px'
+        unit.style.width = areaWidth / num + 'px'
+        gridElement.push(unit)
+    }
 
+    drawingArea.replaceChildren(...gridElement)
+
+    let textOfGridSize = document.querySelector('#grid-size label')
+    textOfGridSize.innerText = `${inputOfGridSize.value} by ${inputOfGridSize.value}`    
 }
 
 
-document.addEventListener('load', () => {
+drawingArea.addEventListener('mouseover', (e) => {
+    if (e.target.className == 'grid-unit') {
+        e.target.style.backgroundColor = 'black';   
+    }
+})
 
+function draw() {
+}
+
+
+document.addEventListener('DOMContentLoaded', () => {
     console.log('loaded');
-    
+    createGrid(inputOfGridSize.value)
 })
